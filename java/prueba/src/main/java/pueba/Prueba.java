236e4024daf5fa0c58533214b57cb9b6276c195a
E016800 <E016800@iberdrola.com>
2018-08-30 13:59:05 +0200

    Entrega 2

--- java/prueba/src/main/java/pueba/Prueba.java
+++ java/prueba/src/main/java/pueba/Prueba.java
@@ -3,6 +3,6 @@ package pueba;
 public class Prueba {
 
 	public static void main(String[] args) {
-		System.out.println("Esto es una prueba modificada 5 aaaaabbbbb");
+		System.out.println("Esto es una prueba modificada 5");
 	}
 }
